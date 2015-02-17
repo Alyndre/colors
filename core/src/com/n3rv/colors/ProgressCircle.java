1f127590dcbf3aa920ae1b3fd8aa5c5ddc61a063
Dani <gomess13@bitbucket.org>
2015-02-17 23:56:46 +0100

    llimpiesa codi ProgressCircle

--- core/src/com/n3rv/colors/ProgressCircle.java
+++ core/src/com/n3rv/colors/ProgressCircle.java
@@ -13,22 +13,14 @@ import com.badlogic.gdx.utils.ShortArray;
 
 public class ProgressCircle extends Image {
 
-    //ProgressCirlce là một hình được cắt bỏ một góc được tạo bởi đoạn thẳng từ tâm tới giữa cạnh  và "cây kim đồng hồ" quay quanh tâm.
-    //"cây kim đồng hồ" ban đầu khi percent = 0 sẽ là đoạn thẳng từ tâm tới giữa cạnh trên.
-    //và quay từ 0 đền 360 độ ứng với percent từ 0 đến 100
-
-    //Hằng số ghi nhận "cây kim đồng hồ" cắt cạnh nào của imgae.
     public enum IntersectAt {
         NONE, TOP, BOTTOM, LEFT, RIGHT
     }
 
-    //region của hình.
     TextureRegion texture;
 
-    //polyBatch để vẽ một phần hình của image.
     PolygonSpriteBatch polyBatch;
 
-    //các điểm đại diện cho tâm, giữa đỉnh trên, left-top, left-bottom, right-top, right-buttom.. và điểm "kim đồng hồ" cắt vào canh.
     Vector2 center;
     Vector2 centerTop;
     Vector2 leftTop;
@@ -37,21 +29,16 @@ public class ProgressCircle extends Image {
     Vector2 rightTop;
     Vector2 progressPoint;
 
-    //danh sách các điểm cần vẽ...
     float[] fv;
 
-    //cờ cho biết "kim đồng hồ" đã cắt canh nào của image..
     IntersectAt intersectAt;
 
-
-    //Hàm khởi tạo - có tham số là region của hình và PolygonSpriteBatch để vẽ....
     public ProgressCircle(TextureRegion region, PolygonSpriteBatch polyBatch) {
         super(region);
 
         this.texture = region;
         this.polyBatch = polyBatch;
 
-        //Định nghĩa các điểm đại diện...
         center = new Vector2(this.getWidth() / 2, this.getHeight() / 2);
         centerTop = new Vector2(this.getWidth() / 2, this.getHeight());
         leftTop = new Vector2(0, this.getHeight());
@@ -60,15 +47,10 @@ public class ProgressCircle extends Image {
         rightTop = new Vector2(this.getWidth(), this.getHeight());
         progressPoint = new Vector2(this.getWidth() / 2, this.getHeight() / 2);
 
-        //khởi đầu cho percent = 0;
         setPercentage(0);
-
 
     }
 
-
-    //Hàm tính "cây kim đồng hổ" (line) - cắt với 1 trong 4 cạnh của imgae.
-    //hàm trả về điểm cắt và cập nhật cạnh được cắt...
     private Vector2 IntersectPoint(Vector2 line) {
         Vector2 v = new Vector2();
         boolean isIntersect;
@@ -104,8 +86,6 @@ public class ProgressCircle extends Image {
         }
     }
 
-
-    //cho giá trị percent để quay "kim đồng hồ"
     public void setPercentage(float percent) {
         //100 % = 360 degree
         //==> percent % => (percent * 360 / 100) degree
@@ -121,9 +101,8 @@ public class ProgressCircle extends Image {
         Vector2 v = IntersectPoint(line);
 
         if (intersectAt == IntersectAt.TOP) {
-            if (v.x >= this.getWidth() / 2) // cắt bên phải cạnh
+            if (v.x >= this.getWidth() / 2)
             {
-                //các điểm sẽ vẽ sẽ là ....
                 fv = new float[]{
                         center.x,
                         center.y,
@@ -141,7 +120,7 @@ public class ProgressCircle extends Image {
                         v.y
                 };
             } else {
-                fv = new float[]{ // cắt bên trái cạnh
+                fv = new float[]{
                         center.x,
                         center.y,
                         centerTop.x,
@@ -195,51 +174,40 @@ public class ProgressCircle extends Image {
 
         } else // if (intersectAt == IntersectAt.NONE)
         {
-            //không cắt thì không vẽ........:-)
             fv = null;
         }
 
 
     }
 
-
-    // override hàm draw để tự vẽ theo cách riêng của mình...
     // @Override
     public void draw(SpriteBatch batch, float parentAlpha) {
         //super.draw(batch, parentAlpha);
 
         if (fv == null) return;
 
-        batch.end(); //nhớ tắt batch để vẽ bằng polyPatch.
+        batch.end();
         drawMe();
-        batch.begin(); //nhớ mở lại như cũ...
+        batch.begin();
 
     }
 
-
-    //Vẽ một phần của hình (có bỏ góc quay của kim đồng hồ)....theo fv
     public void drawMe() {
-        //quy chiếu các điểm tạo polygon sang triangula (tham biến thứ hai crua polygonRegion).
         EarClippingTriangulator e = new EarClippingTriangulator();
         ShortArray sv = e.computeTriangles(fv);
 
-        //tạo polygonRegion.
         PolygonRegion polyReg = new PolygonRegion(texture, fv, sv.toArray());
 
-        //tạo polySprite từ polygonRegion.
         PolygonSprite poly = new PolygonSprite(polyReg);
 
-        //lấy tất cả thuộc tính của image cho poly (position, origin, rotation, color)
         poly.setOrigin(this.getOriginX(), this.getOriginY());
         poly.setPosition(this.getX(), this.getY());
         poly.setRotation(this.getRotation());
         poly.setColor(this.getColor());
 
-        //bắt đầu vẽ poly (với đầy đủ thuộc tính của Image).
         polyBatch.begin();
         poly.draw(polyBatch);
         polyBatch.end();
-
 
     }
 
@@ -249,7 +217,6 @@ public class ProgressCircle extends Image {
 
     float convertToDegrees(float angleInRadians) {
         return angleInRadians * 57.2957795f;
-
     }
 
     float convertToRadians(float angleInDegrees) {
